e308f095e5cb1c74ebd5e5fe27f31a8c1c084687
slimane2018 <babelweb.dz@gmail.com>
2026-07-03 15:21:07 -0400

    Fix missing Link import in Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { login, signup } from '../api';
 
 function Login() {
